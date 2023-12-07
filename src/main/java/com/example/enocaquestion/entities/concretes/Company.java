953ab1408ffc31c07c2b15d1729c1592deb06cbb
ONUR AKKOYUN <akkoyunonurcan@gmail.com>
2023-12-07 20:55:14 +0300

    Update Company.java

--- src/main/java/com/example/enocaquestion/entities/concretes/Company.java
+++ src/main/java/com/example/enocaquestion/entities/concretes/Company.java
@@ -27,22 +27,22 @@ import lombok.NoArgsConstructor;
 @JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
 public class Company {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "company_id")
-	private Long companyId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "company_id")
+    private Long companyId;
 	
-	@Column(name = "company_name")
-	private String companyName;
+    @Column(name = "company_name")
+    private String companyName;
 	
-	@Column(name = "website")
-	private String website;
+    @Column(name = "website")
+    private String website;
 
-	@Column(name = "address")
+    @Column(name = "address")
     private String address;
 
-	@JsonIgnore
-	@OneToMany(mappedBy = "company", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonIgnore
+    @OneToMany(mappedBy = "company", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Employee> employees;
 }
 
